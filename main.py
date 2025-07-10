9a23e91ba37feaad6ef94984a957e74a1423ea4b
Adolfo <adolfogabriel1@hotmail.com>
2025-07-10 16:58:32 -0300

    bootstrap e comunicação python html

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 
 @app.route('/')
-def hello():
-  return "hello"
+def home():
+  return render_template('home.html')
+
+lista_usuarios = ['Adolfo', 'João', 'José', 'Lucas']
+
+@app.route('/usuarios')
+def usuarios():
+  return render_template('usuarios.html', lista_usuarios=lista_usuarios)
   
 if __name__ == "__main__":
   app.run(debug=True)
