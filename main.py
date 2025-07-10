e9a2e923dd6e02f912cd4eac5987676986335797
Adolfo <adolfogabriel1@hotmail.com>
2025-07-10 20:29:49 -0300

    criação navbar, login e criar conta

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__) 
+
+lista_usuarios = ['Adolfo', 'João', 'José', 'Lucas']
 
 @app.route('/')
 def home():
   return render_template('home.html')
 
-lista_usuarios = ['Adolfo', 'João', 'José', 'Lucas']
-
 @app.route('/usuarios')
 def usuarios():
   return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+  
+@app.route('/login')
+def login():
+  return render_template('login.html')
   
 if __name__ == "__main__":
   app.run(debug=True)
